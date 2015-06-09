77bd46392b488751a63471d1e584eda771790e5b
ericvergnaud <eric.vergnaud@wanadoo.fr>
2015-06-10 00:31:33 +0800

    code store interface updated

--- Java-Core/src/main/java/presto/store/ICodeStore.java
+++ Java-Core/src/main/java/presto/store/ICodeStore.java
@@ -1,7 +1,9 @@
 package presto.store;
 
-import presto.runtime.Context;
+import presto.declaration.IDeclaration;
 
+/* a code store is a place where a code consumer (interpreter, compiler...) can fetch code from */
+/* code stores can be chained so that currently edited code can referred to stable code */
 public interface ICodeStore {
-	Context getContext();
+	IDeclaration fetch(String name);
 }
