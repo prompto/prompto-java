05844efd35b0ca94ff3eade46f838497f074d740
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-10-10 00:29:58 +0800

    Revert "support declarations in non global context"
    This reverts commit 5421199c4201f0e34722a007b4b12b4d11e2c6ef.

--- Java-Core/src/main/java/prompto/runtime/Context.java
+++ Java-Core/src/main/java/prompto/runtime/Context.java
@@ -293,8 +293,6 @@ public class Context implements IContext {
 			return actual;
 		if(parent!=null)
 			return parent.getRegistered(name);
-		if(calling!=null && calling!=parent)
-			return calling.getRegistered(name);
 		if(globals!=this)
 			return globals.getRegistered(name);
 		return null;	
@@ -311,10 +309,6 @@ public class Context implements IContext {
 			return ObjectUtils.downcast(klass,actual);
 		else if(parent!=null)
 			actual = parent.getRegisteredDeclaration(klass, name, lookInStore);
-		if(actual!=null)
-			return ObjectUtils.downcast(klass,actual);
-		else if(calling!=null && calling!=parent)
-			actual = calling.getRegisteredDeclaration(klass, name, lookInStore);
 		if(actual!=null)
 			return ObjectUtils.downcast(klass,actual);
 		else if(globals!=this)
