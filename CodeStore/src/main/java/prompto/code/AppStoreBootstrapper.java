38886601fdc1adbfd29bd3b73bac30f4ec2ed620
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-03-13 23:31:11 +0800

    use URLs to identify resource paths

--- CodeStore/src/main/java/prompto/code/AppStoreBootstrapper.java
+++ CodeStore/src/main/java/prompto/code/AppStoreBootstrapper.java
@@ -2,6 +2,7 @@ package prompto.code;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import prompto.code.ICodeStore.ModuleType;
 import prompto.error.ReadWriteError;
@@ -12,21 +13,41 @@ public class AppStoreBootstrapper {
 
 	public static ICodeStore bootstrap(IStore store, ICodeStore runtime, String application, String version, String ... resourceNames) {
 		System.out.println("Connecting to code store for application " + application + " version " + version + "...");
-		if(resourceNames!=null) for(String resourceName : resourceNames) {
-			if(resourceName.startsWith("\""))
-				resourceName = resourceName.substring(1);
-			if(resourceName.endsWith("\""))
-				resourceName = resourceName.substring(0, resourceName.length()-1);
-			File file = new File(resourceName);
-			if(!file.exists())
-				throw new ReadWriteError("Could not locate resource: " + resourceName);
-			try {
-				runtime = new ResourceCodeStore(runtime, ModuleType.WEBSITE, file.toURI().toURL(), version.toString());
-			} catch (MalformedURLException e) {
-				throw new InternalError(e);
+		if(resourceNames!=null) {
+			for(String resourceName : resourceNames) {
+				resourceName = trim(resourceName);
+				runtime = bootstrapResource(resourceName, runtime, version);
 			}
 		}
 		return runtime;
+	}
+
+	private static String trim(String resourceName) {
+		if(resourceName.startsWith("\""))
+			resourceName = resourceName.substring(1);
+		if(resourceName.endsWith("\""))
+			resourceName = resourceName.substring(0, resourceName.length()-1);
+		return resourceName;
+	}
+
+	private static ICodeStore bootstrapResource(String resourceName, ICodeStore runtime, String version) {
+		try {
+			URL resourceUrl = getUrl(resourceName);
+			return new ResourceCodeStore(runtime, ModuleType.WEBSITE, resourceUrl, version);
+		} catch (MalformedURLException e) {
+			throw new InternalError(e);
+		}
+	}
+
+	private static URL getUrl(String resourceName) throws MalformedURLException {
+		if(resourceName.startsWith("file:") || resourceName.startsWith("jar:"))
+			return new URL(resourceName);
+		else {
+			File file = new File(resourceName);
+			if(!file.exists())
+				throw new ReadWriteError("Could not locate resource: " + resourceName);
+			return file.toURI().toURL();
+		}
 	}
 
 }
