2222c0c895fb879351f30b4e7ace46dca1cf6e1d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-04-04 14:01:15 +0800

    cleanup

--- Core/src/main/java/prompto/csharp/CSharpThisExpression.java
+++ Core/src/main/java/prompto/csharp/CSharpThisExpression.java
@@ -1,11 +1,8 @@
 package prompto.csharp;
 
-import prompto.expression.ThisExpression;
 import prompto.utils.CodeWriter;
 
 public class CSharpThisExpression implements CSharpExpression {
-	
-	ThisExpression expression = new ThisExpression();
 	
 	@Override
 	public void toDialect(CodeWriter writer) {
