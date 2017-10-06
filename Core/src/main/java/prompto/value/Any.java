32539d3bc682a48f11acd60c4f5a1c0f0b79fa10
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-10-06 23:30:20 +0800

    Support reading “Any” object from store, should never happen but useful for testing.

---
+++ Core/src/main/java/prompto/value/Any.java
@@ -0,0 +1,15 @@
+package prompto.value;
+
+public class Any {
+
+	Object dbId;
+
+	public void setDbId(Object dbId) {
+		this.dbId = dbId;
+	}
+	
+	public Object getDbId() {
+		return dbId;
+	}
+
+}
