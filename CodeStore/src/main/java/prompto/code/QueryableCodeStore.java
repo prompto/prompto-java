e93bff93d87ddbc6d1a493249fcf0e62e0a0f0f6
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-02-25 11:57:00 +0800

    add store setter

--- CodeStore/src/main/java/prompto/code/QueryableCodeStore.java
+++ CodeStore/src/main/java/prompto/code/QueryableCodeStore.java
@@ -83,6 +83,10 @@ public class QueryableCodeStore extends BaseCodeStore {
 		return store;
 	}
 	
+	public void setStore(IStore store) {
+		this.store = store;
+	}
+	
 	public void collectStorables(List<IStorable> list, IDeclaration declaration, Dialect dialect, PromptoVersion version, Object moduleId) {
 		if(declaration instanceof MethodDeclarationMap) {
 			for(IDeclaration method : ((MethodDeclarationMap)declaration).values())
