e162bc56a30e46f6fd09cc63c28f14c591770316
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-11-04 01:25:47 +0800

    more utils

--- Core/src/main/java/prompto/utils/ThreadUtils.java
+++ Core/src/main/java/prompto/utils/ThreadUtils.java
@@ -1,14 +1,46 @@
 package prompto.utils;
 
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public abstract class ThreadUtils {
 
-	public static Thread[] getActiveThreads() {
-		Thread[] threads = new Thread[Thread.activeCount()];
-		Thread.enumerate(threads);
-		return threads;
+	public static ThreadGroup getRootThreadGroup() {
+	    ThreadGroup result = Thread.currentThread( ).getThreadGroup( );
+	    while(result.getParent()!=null)
+	    	result = result.getParent();
+	    return result;
+	}
+	
+	
+	public static ThreadGroup[] getAllThreadGroups() {
+		ThreadGroup root = getRootThreadGroup();
+		int count = root.activeGroupCount();
+		for(;;) {
+			ThreadGroup[] groups = new ThreadGroup[count * 2];
+			count = root.enumerate(groups, true);
+			if(count==groups.length)
+				continue;
+			ThreadGroup[] result = new ThreadGroup[count + 1];	
+			result[0] = root;
+			System.arraycopy(groups, 0, result, 1, count);
+			return result;
+		}
+	}
+	
+	public static Thread[] getAllThreads() {
+		ThreadGroup root = getRootThreadGroup();
+		long count = ManagementFactory.getThreadMXBean().getTotalStartedThreadCount();
+		for(;;) {
+			Thread[] threads = new Thread[(int)count * 2];
+			count = root.enumerate(threads, true);
+			if(count==threads.length)
+				continue;
+			Thread[] result = new Thread[(int)count];	
+			System.arraycopy(threads, 0, result, 0, (int)count);
+			return result;
+		}
 	}
 
 	public static void removeThreadLocalForThread(Thread thread, ThreadLocal<?> local) throws Exception {
