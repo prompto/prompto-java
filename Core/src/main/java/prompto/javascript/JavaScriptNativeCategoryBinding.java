f10d83ccd12c6308b7679ec2d517aed89b9adaf8
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-12-09 02:25:57 +0800

    fix js runtime code loading

--- Core/src/main/java/prompto/javascript/JavaScriptNativeCategoryBinding.java
+++ Core/src/main/java/prompto/javascript/JavaScriptNativeCategoryBinding.java
@@ -25,9 +25,9 @@ public class JavaScriptNativeCategoryBinding extends NativeCategoryBinding {
 
 	public void transpile(Transpiler transpiler) {
 	    if(this.module!=null) {
-	    	transpiler.append("var ").append(identifier).append(" = require('");
+	    	transpiler.append("var ").append(identifier).append(" = require('/");
 	        this.module.transpile(transpiler);
-	        transpiler.append("').").append(identifier).append(";").newLine();
+	        transpiler.append("', null, null, function(m) { return {id: m, uri: m}; }).").append(identifier).append(";").newLine();
 	    }
 	}
 
