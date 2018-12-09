cb656ffb80e0b372c00e39f4b55613b9f072ca04
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-12-09 14:06:30 +0800

    fix warnings

--- Core/src/test/java/prompto/transpiler/MemStoreMirror.java
+++ Core/src/test/java/prompto/transpiler/MemStoreMirror.java
@@ -25,7 +25,6 @@ import prompto.store.IStoredIterable;
 import prompto.store.memory.MemStore;
 import prompto.store.memory.Query;
 
-@SuppressWarnings("restriction")
 public class MemStoreMirror {
 	
 	MemStore store = new MemStore();
