0605ac85bce8f1e15ba2d56a998caaf45dab320e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-04-15 23:09:14 +0800

    add missing code

--- Core/src/main/resources/prompto/intrinsic/DateTime.js
+++ Core/src/main/resources/prompto/intrinsic/DateTime.js
@@ -5,6 +5,11 @@ function DateTime(date, tzOffset) {
     return this;
 }
 
+DateTime.now = function() {
+    var now = new Date();
+    return new DateTime(now, now.getTimezoneOffset());
+};
+
 DateTime.parse = function(text) {
     var year = parseInt(text.substring(0,4));
     text = text.substring(4);
