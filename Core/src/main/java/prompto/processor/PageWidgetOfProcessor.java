339eacfe181a06e825ab1471613558eaba96f351
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-09-29 18:17:43 +0800

    add missing no-op annotation processor

---
+++ Core/src/main/java/prompto/processor/PageWidgetOfProcessor.java
@@ -0,0 +1,14 @@
+package prompto.processor;
+
+import prompto.declaration.CategoryDeclaration;
+import prompto.grammar.Annotation;
+import prompto.runtime.Context;
+
+public class PageWidgetOfProcessor extends AnnotationProcessor {
+
+	@Override
+	public void processCategory(Annotation annotation, Context context, CategoryDeclaration declaration) {
+		// does nothing
+	}
+
+}
