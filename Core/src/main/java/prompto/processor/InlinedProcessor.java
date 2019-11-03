beb56f4758d7fcd723a08dcef96f614c0867cf3b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-11-03 11:17:03 +0800

    add @Inlined annotation processor to avoid warning

---
+++ Core/src/main/java/prompto/processor/InlinedProcessor.java
@@ -0,0 +1,14 @@
+package prompto.processor;
+
+import prompto.declaration.CategoryDeclaration;
+import prompto.grammar.Annotation;
+import prompto.runtime.Context;
+
+public class InlinedProcessor extends AnnotationProcessor {
+
+	@Override
+	public void processCategory(Annotation annotation, Context context, CategoryDeclaration declaration) {
+		// nothing to do
+	}
+
+}
