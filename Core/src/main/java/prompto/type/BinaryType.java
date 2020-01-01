ac842a50ac7ebeb9522a1eb849475b84a22f21ed
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-01-01 16:01:38 +0100

    fix incorrect binary members

--- Core/src/main/java/prompto/type/BinaryType.java
+++ Core/src/main/java/prompto/type/BinaryType.java
@@ -24,9 +24,9 @@ public abstract class BinaryType extends NativeType {
 	@Override
 	public IType checkMember(Context context, Identifier id) {
 		String name = id.toString();
-		if ("name".equals(name))
+		if ("mimeType".equals(name))
 			return TextType.instance();
-		else if ("format".equals(name))
+		else if ("url".equals(name))
 			return TextType.instance();
 		else
 			return super.checkMember(context, id);
