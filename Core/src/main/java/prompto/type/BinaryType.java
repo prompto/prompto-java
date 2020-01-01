3a349fde7d28cbd3c6392038a893a51bd64dc83f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-01-01 21:14:06 +0100

    refactor

--- Core/src/main/java/prompto/type/BinaryType.java
+++ Core/src/main/java/prompto/type/BinaryType.java
@@ -2,6 +2,7 @@ package prompto.type;
 
 import java.util.Map;
 
+import prompto.error.ReadWriteError;
 import prompto.grammar.Identifier;
 import prompto.intrinsic.PromptoBinary;
 import prompto.runtime.Context;
@@ -42,26 +43,37 @@ public abstract class BinaryType extends NativeType {
 	
 	@Override
 	public IValue readJSONValue(Context context, JsonNode value, Map<String, byte[]> parts) {
-		if(value.isNull())
-			return NullValue.instance();
-		// if this an object received from the browser, use reverse logic from BinaryValue::toJson
-		if(value.isTextual() && value.asText().startsWith("/ws/bin/data?")) {
-			return null; // TODO ok for storing an updated instance (attribute will not be overwritten), but might need a lazy loading BinaryRef value for other situations ? 
-		} else if(value.isTextual() && value.asText().startsWith("@")) {
-			byte[] bytes = parts.get(value.asText());
-			if(bytes==null)
-				return NullValue.instance(); // TODO throw ?
-			String[] segments = value.asText().split("/");
-			String mimeType = segments[segments.length-1].replaceAll("\\.", "/");
-			return newInstance(new PromptoBinary(mimeType, bytes));
-		} else {
-			String partName = value.get("partName").asText();
-			byte[] bytes = parts.get(partName);
-			if(bytes==null)
-				return NullValue.instance(); // TODO throw ?
-			String mimeType = value.get("mimeType").asText();
-			return newInstance(new PromptoBinary(mimeType, bytes));
+		if(value.isObject()) {
+			JsonNode valueNode = value.get("value");
+			if(valueNode!=null && valueNode.isObject() && parts!=null) {
+				// if this an object received by the browser, use reverse logic from BinaryValue::toJson
+				JsonNode urlNode = valueNode.get("url");
+				if(urlNode!=null && urlNode.isTextual()) {
+					String url = urlNode.asText();
+					if(url.startsWith("/ws/bin/data?"))
+						return null; // TODO ok for storing an updated instance (attribute will not be overwritten), but might need a lazy loading BinaryRef value for other situations ? 
+					else if(url.startsWith("@")) {
+						byte[] bytes = parts.get(url);
+						if(bytes==null)
+							return NullValue.instance(); // TODO throw ?
+						String[] segments = url.split("/");
+						String mimeType = segments[segments.length-1].replaceAll("\\.", "/");
+						return newInstance(new PromptoBinary(mimeType, bytes));
+					}
+				}
+				// if this an object created by the browser, image data 
+				JsonNode partNode = valueNode.get("partName");
+				if(partNode!=null && partNode.isTextual()) {
+					String partName = partNode.asText();
+					byte[] bytes = parts.get(partName);
+					if(bytes==null)
+						return NullValue.instance(); // TODO throw ?
+					String mimeType = valueNode.get("mimeType").asText();
+					return newInstance(new PromptoBinary(mimeType, bytes));
+				}
+			}
 		}
+		throw new ReadWriteError("Cannot read binary value from: " + value.toString());
 	}
 	
 }
