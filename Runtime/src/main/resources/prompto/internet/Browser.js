da187d001e279297053ca25eca4d3a275fe93b11
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-02-08 12:52:19 +0800

    rename openWindow -> openBrowser

--- Runtime/src/main/resources/prompto/internet/Browser.js
+++ Runtime/src/main/resources/prompto/internet/Browser.js
@@ -1,6 +1,6 @@
 var window = this.window;
 /* work around limited JS syntax in prompto bindings */
 /* cannot write: window[methodName](url, name) */
-exports.openWindow = function(methodName, url, name) {
+exports.openBrowser = function(methodName, url, name) {
     window[methodName](url, name);
 }
