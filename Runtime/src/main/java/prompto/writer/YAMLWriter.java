4e6e1810e6ddedd7a51d60b1677da12acc6a5002
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-03-04 00:31:51 +0800

    improve formatting

--- Runtime/src/main/java/prompto/writer/YAMLWriter.java
+++ Runtime/src/main/java/prompto/writer/YAMLWriter.java
@@ -3,6 +3,8 @@ package prompto.writer;
 import java.io.IOException;
 import java.io.StringWriter;
 
+import com.esotericsoftware.yamlbeans.YamlConfig;
+import com.esotericsoftware.yamlbeans.YamlConfig.WriteClassName;
 import com.esotericsoftware.yamlbeans.YamlWriter;
 
 import prompto.intrinsic.PromptoDocument;
@@ -12,8 +14,12 @@ public abstract class YAMLWriter {
 
 	public static String write(PromptoList<PromptoDocument<String, Object>> docs) throws IOException {
 		StringWriter yaml = new StringWriter();
-		YamlWriter writer = new YamlWriter(yaml);
-		writer.write(docs);
+		YamlConfig config = new YamlConfig();
+		config.writeConfig.setWriteClassname(WriteClassName.NEVER);
+		config.writeConfig.setWriteRootTags(false);
+		YamlWriter writer = new YamlWriter(yaml, config);
+		Object data = docs.size()==1 ? docs.get(0) : docs;
+		writer.write(data);
 		writer.close();
 		yaml.flush();
 		return yaml.toString();
