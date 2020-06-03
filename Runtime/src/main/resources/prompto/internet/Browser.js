3fd459b72618218ba2c9d155fe0c1e0dd7fd4fd9
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-06-03 22:34:40 +0800

    latest libs

--- Runtime/src/main/resources/prompto/internet/Browser.js
+++ Runtime/src/main/resources/prompto/internet/Browser.js
@@ -1,9 +1,17 @@
 var window = this.window;
+
 /* work around limited JS syntax in prompto bindings */
 /* cannot write: window[methodName](url, name) */
 exports.openBrowser = function(methodName, url, name) {
     window[methodName](url, name);
-}
+};
+
+/* work around limited JS syntax in prompto bindings */
+/* cannot write: window.location = location; */
+exports.browserGoto = function(location) {
+    window.location = location;
+};
+
 
 exports.downloadFile = function(url, name) {
 	var document = window.document;
@@ -13,4 +21,5 @@ exports.downloadFile = function(url, name) {
 	document.body.appendChild(a);
 	a.click();
 	document.body.removeChild(a);
-}
+};
+
