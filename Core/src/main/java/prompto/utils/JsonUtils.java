cb03e147a648393eed59fe45e443117e65ae095b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-07-20 00:07:44 +0800

    more utils

--- Core/src/main/java/prompto/utils/JsonUtils.java
+++ Core/src/main/java/prompto/utils/JsonUtils.java
@@ -2,20 +2,36 @@ package prompto.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public abstract class JsonUtils {
 
-	public static JsonNode readResource(String resource) throws IOException {
+	public static JsonNode parseResource(String resource) throws IOException {
 		try(InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource)) {
-			return new ObjectMapper().readTree(input);
+			return parseInput(input);
 		}
 	}
-	
+
+	public static JsonNode parseInput(InputStream input) throws IOException {
+		return new ObjectMapper().readTree(input);
+	}
+
+	public static JsonNode parseString(String data) throws IOException {
+		return new ObjectMapper().readTree(data);
+	}
+
 	public static String objectToJson(Object object) throws IOException {
 		return new ObjectMapper().writeValueAsString(object);
 	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> toMap(ObjectNode node) {
+		return new ObjectMapper().convertValue(node, Map.class);
+	}
+
 
 }
