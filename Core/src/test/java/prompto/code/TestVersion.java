b9f065fa024d0701483ba8d5e538758a9258146f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-07-19 19:30:51 +0800

    renamed

--- Core/src/test/java/prompto/code/TestVersion.java
+++ Core/src/test/java/prompto/code/TestVersion.java
@@ -9,7 +9,7 @@ import prompto.intrinsic.PromptoVersion;
 public class TestVersion {
 
 	@Test
-	public void test() {
+	public void preservesValueOverRoundtrip() {
 		String s = "1.2.3";
 		PromptoVersion v = PromptoVersion.parse(s);
 		assertEquals(s, v.toString());
