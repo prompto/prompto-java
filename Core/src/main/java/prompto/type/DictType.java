65fe52cceb87aa8795b7a7a144761d581b037394
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-11-24 21:05:38 +0800

    fix warning

--- Core/src/main/java/prompto/type/DictType.java
+++ Core/src/main/java/prompto/type/DictType.java
@@ -2,7 +2,6 @@ package prompto.type;
 
 import java.lang.reflect.Type;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
