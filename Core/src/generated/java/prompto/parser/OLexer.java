3c87e21ec5e456068ba095ce4462afd4236c1334
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-11-25 13:53:52 +0800

    upgrade antlr4 to 4.9

--- Core/src/generated/java/prompto/parser/OLexer.java
+++ Core/src/generated/java/prompto/parser/OLexer.java
@@ -1,4 +1,4 @@
-// Generated from OLexer.g4 by ANTLR 4.8
+// Generated from OLexer.g4 by ANTLR 4.9
 package prompto.parser;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class OLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
