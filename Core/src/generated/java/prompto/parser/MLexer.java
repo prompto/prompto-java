7e8bae043255ecd8ad60ebd74990459689b60c55
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-06 09:39:42 +0800

    Upgrade ANTLR to 4.9.1

--- Core/src/generated/java/prompto/parser/MLexer.java
+++ Core/src/generated/java/prompto/parser/MLexer.java
@@ -1,4 +1,4 @@
-// Generated from MLexer.g4 by ANTLR 4.9
+// Generated from MLexer.g4 by ANTLR 4.9.1
 package prompto.parser;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.9", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9.1", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
--- Core/src/generated/java/prompto/utils/ArgsParserListener.java
+++ Core/src/generated/java/prompto/utils/ArgsParserListener.java
@@ -1,4 +1,4 @@
-// Generated from ArgsParser.g4 by ANTLR 4.9
+// Generated from ArgsParser.g4 by ANTLR 4.9.1
 package prompto.utils;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
