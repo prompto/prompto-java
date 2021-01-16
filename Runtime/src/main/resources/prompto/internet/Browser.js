a37fd70e5a81d3a850ef58976641b42e6c309af4
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-16 11:43:42 +0800

    return the opened tab

--- Runtime/src/main/resources/prompto/internet/Browser.js
+++ Runtime/src/main/resources/prompto/internet/Browser.js
@@ -3,7 +3,7 @@ var window = this.window;
 /* work around limited JS syntax in prompto bindings */
 /* cannot write: window[methodName](url, name) */
 exports.openBrowser = function(methodName, url, name) {
-    window[methodName](url, name);
+    return window[methodName](url, name);
 };
 
 /* work around limited JS syntax in prompto bindings */
