74094fbad3d2d7bb48ad4e9ad08efd2a65264a03
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-02 12:39:16 +0800

    fix missing arg in websocket

--- Runtime/src/main/resources/prompto/web/WebSocket.js
+++ Runtime/src/main/resources/prompto/web/WebSocket.js
@@ -1,14 +1,16 @@
-function openWebSocket(url, onMessage, onClose, callback) {
+ exports.openWebSocket = function(url, onMessage, onClose, callback) {
     var webSocket = new window.WebSocket(url);
     webSocket.onopen = function(event) {
     	webSocket.onmessage = function(event) {
     		if(event.type==="message" && event.data) {
-    			var value = event.data;
-    			if(typeof(value)===typeof(""))
-    				value = JSON.parse(value);
-    			value = readJSONValue(value);	 
-	    		if(onMessage)
+    			if(onMessage) {
+	    			var value = event.data;
+	    			if(typeof(value)===typeof(""))
+	    				value = JSON.parse(value);
+	    			value = readJSONValue(value);	 
 	    			onMessage(value);
+    			} else
+    				console.log("Received message: " + event.data);
 			}
     	};
     	webSocket.onclose = function(event) {
