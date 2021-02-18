2a2d78c668cde5c2fe4878360e2f7c7611a88d9f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-18 21:12:22 +0800

    drop tokenIndex in location equality - unreliable

--- Core/src/main/java/prompto/parser/ILocation.java
+++ Core/src/main/java/prompto/parser/ILocation.java
@@ -3,12 +3,12 @@ package prompto.parser;
 public interface ILocation {
 	
 	static ILocation first(ILocation l1, ILocation l2) {
-		// assumption is that both locations come fromthe same parsing
+		// assumption is that both locations come from the same parsing
 		return l1.getTokenIndex() < l2.getTokenIndex() ? l1 : l2;
 	}
 
 	static ILocation last(ILocation l1, ILocation l2) {
-		// assumption is that both locations come fromthe same parsing
+		// assumption is that both locations come from the same parsing
 		return l1.getTokenIndex() > l2.getTokenIndex() ? l1 : l2;
 	}
 
@@ -17,22 +17,14 @@ public interface ILocation {
 	int getColumn();
 	
 	default boolean isNotAfter(ILocation other) {
-		if(this.getTokenIndex() > -1 && other.getTokenIndex() > -1) {
-			// assumption is that both locations come fromthe same parsing
-			return this.getTokenIndex() <= other.getTokenIndex();
-		} else
-			return this.getLine()<other.getLine()
-					|| (this.getLine()==other.getLine() && this.getColumn()<=other.getColumn());
+		return this.getLine()<other.getLine()
+				|| ( this.getLine()==other.getLine() && this.getColumn()<=other.getColumn() );
 	
 	}
 	
 	default boolean isNotBefore(ILocation other) {
-		if(this.getTokenIndex() > -1 && other.getTokenIndex() > -1) {
-			// assumption is that both locations come fromthe same parsing
-			return this.getTokenIndex() >= other.getTokenIndex();
-		} else
-			return this.getLine()>other.getLine()
-					|| (this.getLine()==other.getLine() && this.getColumn()>=other.getColumn());
+		return this.getLine()>other.getLine()
+				|| ( this.getLine()==other.getLine() && this.getColumn()>=other.getColumn() );
 			
 	}
 	
