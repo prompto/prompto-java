cd0268fd8608bf9a4d637fcfe8c2953677e8a2ea
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-12 11:37:51 +0800

    bump antlr version

--- Core/src/generated/java/prompto/utils/ArgsLexer.java
+++ Core/src/generated/java/prompto/utils/ArgsLexer.java
@@ -1,4 +1,4 @@
-// Generated from ArgsLexer.g4 by ANTLR 4.9.1
+// Generated from ArgsLexer.g4 by ANTLR 4.9.2
 package prompto.utils;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class ArgsLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.9.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
