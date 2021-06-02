043424fa956de4bbffa40f52bde34c6a45d9c9bb
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-06-02 16:09:05 +0200

    add sysGetEnvironmentVariable

---
+++ Runtime/src/test/java/prompto/library/e/TestSystem.java
@@ -0,0 +1,34 @@
+package prompto.library.e;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import prompto.parser.e.BaseEParserTest;
+import prompto.runtime.utils.Out;
+
+public class TestSystem extends BaseEParserTest {
+
+	@Before
+	public void before() throws Exception {
+		Out.init();
+		loadDependency("system");
+	}
+
+	@After
+	public void after() {
+		Out.restore();
+	}
+
+	@Test
+	public void testInterpretedSystem() throws Exception {
+		runInterpretedTests("system/system.pec");
+	}
+
+	@Test
+	public void testCompiledSystem() throws Exception {
+		runCompiledTests("system/system.pec");
+	}
+
+}
+
