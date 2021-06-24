5c8cd075c0e0125fbb12fdb2d0a7c965a8d40423
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-06-24 11:36:20 +0200

    more libs

--- Runtime/src/main/java/prompto/path/Path.java
+++ Runtime/src/main/java/prompto/path/Path.java
@@ -1,12 +1,20 @@
 package prompto.path;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import prompto.utils.OS;
 
@@ -48,7 +56,44 @@ public abstract class Path {
 	}
 
 	public static boolean pathIsLink(String path) {
-		return Files.isSymbolicLink(java.nio.file.Paths.get(path));
+		return Files.isSymbolicLink(Paths.get(path));
 	}
+	
+	public static String compressToTempPath(String path) throws IOException {
+		try(InputStream input = new FileInputStream(Paths.get(path).toFile())) {
+			java.nio.file.Path compressedPath = Files.createTempFile("deflate", ".gz");
+			try(OutputStream output = new FileOutputStream(compressedPath.toFile())) {
+				try(OutputStream deflated = new GZIPOutputStream(output)) {
+					byte[] data = new byte[0x10000];
+					for(;;) {
+						int read = input.read(data);
+						if(read < 0)
+							break;
+						deflated.write(data, 0, read);
+					}
+					return compressedPath.toString();
+				}
+			}
+		}
+	}
+	
+	public static String decompressToTempPath(String path) throws IOException {
+		try(InputStream input = new FileInputStream(Paths.get(path).toFile())) {
+			try(InputStream inflated = new GZIPInputStream(input)) {
+				java.nio.file.Path rawPath = Files.createTempFile("inflate", ".raw");
+				try(OutputStream output = new FileOutputStream(rawPath.toFile())) {
+					byte[] data = new byte[0x10000];
+					for(;;) {
+						int read = inflated.read(data);
+						if(read < 0)
+							break;
+						output.write(data, 0, read);
+					}
+					return rawPath.toString();
+				}
+			}
+		}
+	}
+
 
 }
