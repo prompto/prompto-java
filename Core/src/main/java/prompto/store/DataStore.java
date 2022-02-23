37efc75fd92212f45c05082d4992185cc555155c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-02-23 16:29:52 +0100

    Fix warnings

--- Core/src/main/java/prompto/store/DataStore.java
+++ Core/src/main/java/prompto/store/DataStore.java
@@ -17,7 +17,6 @@ public abstract class DataStore {
 		globalInstance.set(store);
 	}
 
-	@SuppressWarnings("resource")
 	public static void useGlobal() {
 		threadInstance.set(globalInstance.get());
 	}
